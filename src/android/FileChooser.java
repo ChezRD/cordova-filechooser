38b36909a9f696b6d16bf42e285ed48a8b796c0e
Chez <chezrd@gmail.com>
2019-12-12 22:53:54 +0300

    working build

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -1,9 +1,12 @@
 package com.megster.cordova;
 
 import android.app.Activity;
+import android.os.Build;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -19,7 +22,6 @@ public class FileChooser extends CordovaPlugin {
     private static final int PICK_FILE_REQUEST = 1;
 
     public static final String MIME = "mime";
-    public static final String EXTRA_MIME = "extra_mime";
     public static final String PICKER_TITLE = "picker_title";
 
     CallbackContext callback;
@@ -37,19 +39,41 @@ public class FileChooser extends CordovaPlugin {
     }
 
     public void chooseFile(JSONObject filter, CallbackContext callbackContext) {
-        String uri_filter = filter.has(MIME) ? filter.optString(MIME) : "*/*";
-        String additional_uri_filter = filter.has(EXTRA_MIME) ? filter.optString(EXTRA_MIME) : "";
+        ArrayList<String> mimeList = new ArrayList<>();
+        JSONArray mimeTypesArray = filter.has(MIME) ? filter.optJSONArray(MIME) : new JSONArray();
         String select_title = filter.has(PICKER_TITLE) ? filter.optString(PICKER_TITLE) : "Select File";
 
-        // type and title should be configurable
+        for (int j = 0; j < mimeTypesArray.length(); j++) {
+            try {
+                mimeList.add(mimeTypesArray.getString(j));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String baseMime = mimeList.get(0);
+        mimeList.remove(0);// unshift kinda
+        String[] extraMime = mimeList.toArray(new String[mimeList.size()]);
 
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType(uri_filter);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            intent.setType(baseMime.length() > 0 ? baseMime : "*/*");
+
+            if (extraMime.length > 1) {
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, extraMime);
+            }
+        } else {
+            String mimeTypesStr = "";
+            mimeTypesStr += baseMime + "|";
+            for (String mimeType : extraMime) {
+                mimeTypesStr += mimeType + "|";
+            }
+            intent.setType(mimeTypesStr.substring(0, mimeTypesStr.length() - 1));
+        }
+
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-        if (additional_uri_filter.length() > 0) {
-            intent.putExtra(Intent.EXTRA_MIME_TYPES, additional_uri_filter);
-        }
 
         Intent chooser = Intent.createChooser(intent, select_title);
         cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
